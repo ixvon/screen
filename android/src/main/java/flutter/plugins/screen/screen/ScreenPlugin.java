d834e4a6d0e46fc85bc5e8ade1e766929ef97896
Piotr Bieganowski <piotr.bieganowski@elzab.pl>
2022-11-17 08:43:47 +0100

    main

---
+++ android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
@@ -0,0 +1,120 @@
+package flutter.plugins.screen.screen;
+
+import android.provider.Settings;
+import android.view.WindowManager;
+
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
+
+/**
+ * ScreenPlugin
+ */
+public class ScreenPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+
+//  public ScreenPlugin(PluginRegistry.Registrar registrar){
+//    this._registrar = registrar;
+//  }
+
+  public ScreenPlugin(){
+  }
+//  private PluginRegistry.Registrar _registrar;
+  private MethodChannel channel;
+  private FlutterPluginBinding flutterPluginBinding;
+  private ActivityPluginBinding activityPluginBinding;
+
+//  public static void registerWith(@NonNull PluginRegistry.Registrar registrar) {
+//    final MethodChannel channel = new MethodChannel(registrar.messenger(), "github.com/rozar-ruan/flutter_screen");
+//    channel.setMethodCallHandler(new ScreenPlugin(registrar));
+//  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, Result result) {
+
+    switch(call.method){
+      case "brightness":
+        result.success(getBrightness());
+        break;
+      case "setBrightness":
+        double brightness = call.argument("brightness");
+        WindowManager.LayoutParams layoutParams = activityPluginBinding.getActivity().getWindow().getAttributes();
+        layoutParams.screenBrightness = (float)brightness;
+        activityPluginBinding.getActivity().getWindow().setAttributes(layoutParams);
+        result.success(null);
+        break;
+      case "isKeptOn":
+        int flags = activityPluginBinding.getActivity().getWindow().getAttributes().flags;
+        result.success((flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) != 0) ;
+        break;
+      case "keepOn":
+        Boolean on = call.argument("on");
+        if (on) {
+          System.out.println("Keeping screen on ");
+          activityPluginBinding.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+        else{
+          System.out.println("Not keeping screen on");
+          activityPluginBinding.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+        result.success(null);
+        break;
+
+      default:
+        result.notImplemented();
+        break;
+    }
+  }
+
+  private float getBrightness(){
+    float result = activityPluginBinding.getActivity().getWindow().getAttributes().screenBrightness;
+    if (result < 0) { // the application is using the system brightness
+      try {
+        result = Settings.System.getInt(flutterPluginBinding.getApplicationContext().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float)255;
+      } catch (Settings.SettingNotFoundException e) {
+        result = 1.0f;
+        e.printStackTrace();
+      }
+    }
+    return result;
+  }
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    flutterPluginBinding=binding;
+    channel = new MethodChannel(binding.getBinaryMessenger(), "github.com/rozar-ruan/flutter_screen");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+
+  }
+
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    activityPluginBinding = binding;
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
+  }
+}
